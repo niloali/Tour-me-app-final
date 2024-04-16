f889176089f71d9721827a5e55a0bace7275b99f
fahim <fahim.amiri21@gmail.com>
2024-04-15 23:34:18 -0700

    Buttons added and login page is complated xpect linking the page with sign up

--- TourMeApp-Final_project/TourMeApp-Final_project/Components/InputView.swift
+++ TourMeApp-Final_project/TourMeApp-Final_project/Components/InputView.swift
@@ -2,17 +2,44 @@
 //  InputView.swift
 //  TourMeApp-Final_project
 //
-//  Created by patron on 4/15/24.
-//
+//  Created by Fahim Amiri on 4/15/24.
+//The reason why i have my input view on this folder
+//is for the re use able purpose feel free to follow your own method on the page your going to  under take @Nilo. 
+//these listed input field should us as standard for any inputfield in this project . 
 
 import SwiftUI
 
 struct InputView: View {
+    
+    
+    
+    @Binding
+    
+    var text: String
+    let title: String
+    let placeholder :String
+    var isSecureField = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 12){
+            Text(title)
+                .foregroundStyle(Color(.darkGray))
+                .fontWeight(.semibold)
+                .font(.footnote)
+            
+            if isSecureField{
+                SecureField(placeholder, text: $text)
+                    .font(.system(size: 14))
+            }
+            else{
+                SecureField(placeholder, text: $text)
+                    .font(.system(size: 14))
+            }
+            Divider()
+        }
+        
     }
 }
 
 #Preview {
-    InputView()
+    InputView(text: .constant(""), title: "Email", placeholder:"name@example.com")
 }
--- TourMeApp-Final_project/TourMeApp-Final_project/Core-viewmodels/Authentication/LoginView .swift
+++ TourMeApp-Final_project/TourMeApp-Final_project/Core-viewmodels/Authentication/LoginView .swift
@@ -8,8 +8,88 @@
 import SwiftUI
 
 struct LoginView_: View {
+    //need state binding
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack{
+                //Image on the top
+                //importing the image form our assets
+                Image("icon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 120)
+                    .padding(.vertical, 32)
+                //Text Inputs form feilds
+                //imported or call like  a function form Input view along with its properties.
+                VStack(spacing :24){
+                    InputView(text: $email, title:"Email Address ", placeholder: "name@exmple.com")
+                        .padding(.horizontal, 25.0)
+                        .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                    
+                    //passed form main view or inputview
+                    InputView(text: $password,
+                              title: "Password",
+                              placeholder: "your password",
+                              isSecureField: true)
+                    .padding(.horizontal, 25.0)
+                    
+                }
+                .padding(.horizontal)
+                .padding(.top, 12)
+                
+                //buttons are coded here
+                //Sign in button
+                Button {
+                    print("log user in..")
+                }label: {
+                    HStack{
+                        Text("SIGN IN")
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: UIScreen.main.bounds.width-50, height: 48)
+                }
+                .background(Color(.systemOrange))
+                .cornerRadius(10)
+                .padding(.top,25 )
+                //SIGN UP in button
+                Button {
+                    print("log user in..")
+                }label: {
+                    HStack{
+                        Text("SIGN UP")
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: UIScreen.main.bounds.width-50, height: 48)
+                }
+                .background(Color(.systemOrange))
+                .cornerRadius(10)
+                .padding(.top, 5 )
+                
+                //give space for image.
+                Spacer()
+                
+                //link button to sign up page
+                
+                NavigationLink{
+                    //name of the page
+                    //terms and agreement
+                }label: {
+                    HStack(spacing: 3){
+                        Text("Don't have an account ")
+                        Text("Sign Up")
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundStyle(Color.gray)
+                    }
+                    .font(.system(size: 14))
+                    .foregroundColor(Color.gray)
+                }
+               
+            }
+        }
     }
 }
 
