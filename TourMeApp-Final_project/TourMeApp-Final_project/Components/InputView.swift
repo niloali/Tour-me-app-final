34fea1380da6b653441bfcc7e87089f84f0cb254
fahim <fahim.amiri21@gmail.com>
2024-04-15 22:40:33 -0700

    email and password input added i will next work on buttons

---
+++ TourMeApp-Final_project/TourMeApp-Final_project/Components/InputView.swift
@@ -0,0 +1,18 @@
+//
+//  InputView.swift
+//  TourMeApp-Final_project
+//
+//  Created by patron on 4/15/24.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputView()
+}
