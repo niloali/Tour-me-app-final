158da145f0f043a1564b63ed0d1ff781320a3f71
fahim <fahim.amiri21@gmail.com>
2024-04-15 21:35:41 -0700

    Folders and file created

---
+++ TourMeApp-Final_project/TourMeApp-Final_project/App/TourMeApp_Final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourMeApp_Final_projectApp.swift
+//  TourMeApp-Final_project
+//
+//  Created by patron on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct TourMeApp_Final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
