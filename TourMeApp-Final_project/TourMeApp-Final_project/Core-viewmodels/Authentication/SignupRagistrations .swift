e35982ebcf1d7bb866b98c5c3f8ec29234cf6fe2
fahim <fahim.amiri21@gmail.com>
2024-04-16 00:04:06 -0700

    Sign Up page added and ready to be linked

---
+++ TourMeApp-Final_project/TourMeApp-Final_project/Core-viewmodels/Authentication/SignupRagistrations .swift
@@ -0,0 +1,18 @@
+//
+//  SignupRagistrations .swift
+//  TourMeApp-Final_project
+//
+//  Created by patron on 4/15/24.
+//
+
+import SwiftUI
+
+struct SignupRagistrations_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignupRagistrations_()
+}
