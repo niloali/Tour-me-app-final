2a8f059aeb699192e1c5a0c3c6abcbf2580d14cb
fahim <fahim.amiri21@gmail.com>
2024-04-22 23:17:57 -0700

    profile page is added

---
+++ TourMeApp-Final_project/TourMeApp-Final_project/Core-viewmodels/Profile/profile.swift
@@ -0,0 +1,18 @@
+//
+//  profile.swift
+//  TourMeApp-Final_project
+//
+//  Created by patron on 4/22/24.
+//
+
+import SwiftUI
+
+struct profile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    profile()
+}
