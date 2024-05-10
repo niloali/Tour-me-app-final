505721cef5161a40cdca5bd3d31f3d4f672a432f
fahim <fahim.amiri21@gmail.com>
2024-05-10 00:59:53 -0700

    links added

---
+++ TourMeApp-Final_project/TourMeApp-Final_project/Core-viewmodels/Profile/profileView.swift
@@ -0,0 +1,18 @@
+//
+//  profileView.swift
+//  TourMeApp-Final_project
+//
+//  Created by patron on 5/10/24.
+//
+
+import SwiftUI
+
+struct profileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    profileView()
+}
